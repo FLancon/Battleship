4b9eb3631421504044d993d6d33f734be7db9bd0
Florian Lançon <florian.lancon@gmail.com>
2021-09-23 15:58:37 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    include './includes/dbconnect.local.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Battleship</title>
+</head>
+<body>
+    <div id="test">fgdshfsdjkhfksdhfjksdhfsdkfhs</div>
+fbsd;fb,sdhsfjksdhfjksdh
+</body>
+</html>
